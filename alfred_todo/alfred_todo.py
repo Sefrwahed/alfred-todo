0c735419a702f75712ab5eba26bea4daa272f520
Mohamed Anwer <mohammed.ahmed.anwer@gmail.com>
2017-05-09 19:49:53 +0200

    Adapting api new componenets and styles

--- alfred_todo/alfred_todo.py
+++ alfred_todo/alfred_todo.py
@@ -1,5 +1,5 @@
 from alfred.modules.api.a_base_module import ABaseModule
-
+from alfred.modules.api.view_components import AChecklist, AForm, ATextField, ATextField, ADivider
 from .models.todo_list_item import TodoListItem
 
 
@@ -12,17 +12,44 @@ class AlfredTodo(ABaseModule):
         self.todo_list = TodoListItem.all()
 
     def construct_view(self):
-        self.render_template('List.html', list_items=self.todo_list)
+        self.add_component(AForm(
+            True,
+            "add_circle_outline",
+            ATextField("new_item", "New Item"),
+            id="new_item_form"
+        ))
+        self.add_component(AChecklist(
+            list(map(
+                lambda item: (item.text, bool(item.status), item.id),
+                self.todo_list
+            ))
+        ))
 
-    def on_new_item_submitted(self, value):
-        if value == "":
-            return
-
+    def on_new_item_form_submitted(self, form_data):
         item = TodoListItem()
 
-        item.text = value
+        if form_data.get("new_item") == "":
+            return
+
+        item.text = form_data["new_item"]
         item.status = 0
         item.save()
 
-        self.todo_list.append(item)
-        self.render_template('List.html', list_items=self.todo_list)
+        self.components = []
+        self.callback()
+        self.construct_view()
+        self.populate_view()
+
+    def on_checkbox_click(self, attrs):
+        item = TodoListItem.find(int(attrs['data-id']))
+        item.status = 1 if item.status == 0 else 0
+        item.save()
+
+
+    def on_i_click(self, attrs):
+        if attrs.get('data-action') == "delete":
+            TodoListItem.find(int(attrs['data-id'])).delete()
+            self.components = []
+            self.callback()
+            self.construct_view()
+            self.populate_view()
